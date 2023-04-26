b88813eada101ed29937f9f3846f768d92ad49a6
gavin.pawlovich <gsmallovich@gmail.com>
2023-04-26 01:27:59 -0500

    Started to add in a function to draw a grid with a set number of functions, started to observe stretching issue that the original had... next goal should be to work out exactly why and correct it

--- windowManagement.c
+++ windowManagement.c
@@ -3,6 +3,9 @@
 
 #define DEFAULT_WIN_HEIGHT 1000
 #define DEFAULT_WIN_WIDTH 1000
+#define MIN_WIN_HEIGHT 250
+#define MIN_WIN_WIDTH 250
+#define DEFAULT_GRID_DIVISIONS 25
 
 // structure for basic window data
 typedef struct XWinData
@@ -21,57 +24,64 @@ typedef struct XWinData
     XColor grey;
 }XWinData;
 
-typedef struct bwGridWinData
-{
-    XWinData windowData;
-    char** Grid;
-}bwGridWinData;
-
 // with the eventual intent to come up with a colored version
-// TODO, note that original setup did not scale correctly with window
-bwGridWinData* createBlackWhiteGridWindow(int h, int w, char* name)
+XWinData* createNewWindow(int h, int w, char* name)
 {
-    bwGridWinData* newWindow = calloc(sizeof(bwGridWinData), 1);
+    XWinData* newWindow = calloc(sizeof(XWinData), 1);
 
-    newWindow->windowData.display = XOpenDisplay(NULL);
+    newWindow->display = XOpenDisplay(NULL);
 
     // if failed to get display, return error state to caller
-    if(newWindow->windowData.display == NULL) return NULL;
+    if(newWindow->display == NULL) return NULL;
 
-    newWindow->windowData.screen = DefaultScreen(newWindow->windowData.display);
+    newWindow->screen = DefaultScreen(newWindow->display);
 
     // creating the window itself, also sets some basic info such as the bg and foreground colors, scale, etc
     // colors other than the BG and FG colors are usable once a color map is defined later on
-    newWindow->windowData.window =
-            XCreateSimpleWindow(newWindow->windowData.display,
-                                RootWindow(newWindow->windowData.display, newWindow->windowData.screen),
+    newWindow->window =
+            XCreateSimpleWindow(newWindow->display,
+                                RootWindow(newWindow->display, newWindow->screen),
                                 0, 0, 												                            // window starting x and y location
-                                DEFAULT_WIN_HEIGHT, DEFAULT_WIN_WIDTH, 						                    // window starting height and width values
+                                h >= MIN_WIN_HEIGHT ? h:DEFAULT_WIN_HEIGHT,
+                                w >= MIN_WIN_WIDTH ? w:DEFAULT_WIN_WIDTH,
                                 0, 												                                // window border thickness
-                                BlackPixel(newWindow->windowData.display, newWindow->windowData.screen), 		// foreground color
-                                WhitePixel(newWindow->windowData.display, newWindow->windowData.screen));		// background color
+                                BlackPixel(newWindow->display, newWindow->screen), 		// foreground color
+                                WhitePixel(newWindow->display, newWindow->screen));		// background color
                                 
     // setting window name, for style points obviously	
-    XStoreName(newWindow->windowData.display, newWindow->windowData.window, name);
+    XStoreName(newWindow->display, newWindow->window, name);
 
     // we want to process clicks and keyboard input, as well as when the window becomes visible or is resized
-    XSelectInput(newWindow->windowData.display, newWindow->windowData.window, ExposureMask | KeyPressMask | ButtonPressMask);
+    XSelectInput(newWindow->display, newWindow->window, ExposureMask | KeyPressMask | ButtonPressMask);
 
     // GC = Graphics context
-    newWindow->windowData.graphicsContext = XCreateGC(newWindow->windowData.display, newWindow->windowData.window, 0, NULL);
+    newWindow->graphicsContext = XCreateGC(newWindow->display, newWindow->window, 0, NULL);
 
     // getting color set up in window data structure, XAllocNamedColor can return an error value... TODO, handle this
-    // TODO not sure this needs to be attached to the window itself given that it appears to be based on display and screen which will be shared
-    newWindow->windowData.colormap = DefaultColormap(newWindow->windowData.display, newWindow->windowData.screen);
-    XAllocNamedColor(newWindow->windowData.display, newWindow->windowData.colormap, "red", &newWindow->windowData.red, &newWindow->windowData.red);
-    XAllocNamedColor(newWindow->windowData.display, newWindow->windowData.colormap, "green", &newWindow->windowData.green, &newWindow->windowData.green);
-    XAllocNamedColor(newWindow->windowData.display, newWindow->windowData.colormap, "blue", &newWindow->windowData.blue, &newWindow->windowData.blue);
-    XAllocNamedColor(newWindow->windowData.display, newWindow->windowData.colormap, "gray92", &newWindow->windowData.grey, &newWindow->windowData.grey);
+    newWindow->colormap = DefaultColormap(newWindow->display, newWindow->screen);
+    XAllocNamedColor(newWindow->display, newWindow->colormap, "red", &newWindow->red, &newWindow->red);
+    XAllocNamedColor(newWindow->display, newWindow->colormap, "green", &newWindow->green, &newWindow->green);
+    XAllocNamedColor(newWindow->display, newWindow->colormap, "blue", &newWindow->blue, &newWindow->blue);
+    XAllocNamedColor(newWindow->display, newWindow->colormap, "gray92", &newWindow->grey, &newWindow->grey);
 
     // map window to screen, won't actually appear until first Expose event is handled
-    XMapWindow(newWindow->windowData.display, newWindow->windowData.window);
-
-    //TODO, add grid creation to this...
+    XMapWindow(newWindow->display, newWindow->window);
 
     return newWindow;
+}
+
+// TODO, this function exhibits the same odd behavior on the right and bottom edges that the original did
+void drawEvenGrid(XWinData* windowData, int32_t divisions)
+{
+    // error checking
+    if(divisions <= 0) divisions = DEFAULT_GRID_DIVISIONS;
+    for(int i = 1; i < divisions; i++)
+    {
+        XDrawLine(windowData->display, windowData->window, windowData->graphicsContext,
+                  i*(windowData->windowAttributes.width / divisions), 0,
+                  i*(windowData->windowAttributes.width / divisions), windowData->windowAttributes.height);
+        XDrawLine(windowData->display, windowData->window, windowData->graphicsContext,
+                  0,i*(windowData->windowAttributes.height / divisions),
+                  windowData->windowAttributes.width, i*(windowData->windowAttributes.height / divisions));
+    }
 }
